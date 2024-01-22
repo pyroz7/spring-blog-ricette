cd6cced1bb07266898a6fab2cc057d49b37d7fdb
pyroz7 <vincipyroz7@gmail.com>
2024-01-22 16:36:41 +0100

    Final Commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO category(name)VALUES('Appetizers');
+INSERT INTO category(name)VALUES('First Course');
+INSERT INTO category(name)VALUES('Second Course');
+INSERT INTO category(name)VALUES('Side Orders');
+INSERT INTO category(name)VALUES('Dessert');
+INSERT INTO recipes(category_id, title, ingredients, photo, prepar_time, number_of_portions,`text`) VALUES ( 3,'Salsicce e friarielli', '4 Salsicce e friarielli piccanti, 500g di friarielli, 1 peperoncino frsco, 125g di Vino bianco, 1 Aglio a spicchio', 'https://www.lucianopignataro.it/wp-content/uploads/2010/03/salsicce-e-friarielli.jpg', 90, 6, 'Salsicce è friarielli è uno dei piatti tipici napoletani,una ricetta cosi semplice,ma cosi iconica.');
+INSERT INTO recipes(category_id, title, ingredients, photo, prepar_time, number_of_portions,`text`) VALUES ( 2,'Gateau di patate', '300g di Patate ,100g Fior di latte, 50g salame piccante ,50g prosciutto cotto ,2 Uova, Parmiggiano reggiano, Sale fino , Pepe nero ', 'https://wips.plug.it/cips/buonissimo.org/cms/2020/09/gateau-patate.jpg?w=713&a=c&h=407', 35, 4, 'Il gattò di patate è una torta salata a base di patate tipica della cucina napoletana.');
