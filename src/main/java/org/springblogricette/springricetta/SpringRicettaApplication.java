8f6c0dbf06a5851ca910d735d297b9e29dc838e4
pyroz7 <vincipyroz7@gmail.com>
2024-01-22 09:25:33 +0100

    Inizio progetto

---
+++ src/main/java/org/springblogricette/springricetta/SpringRicettaApplication.java
@@ -0,0 +1,13 @@
+package org.springblogricette.springricetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRicettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRicettaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springblogricette/springricetta/SpringRicettaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springblogricette.springricetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRicettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
